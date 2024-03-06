c4e5f4f3f64c025612b37c4cf9ecc3fc751f3441
Diego Massarni <webdiegomassarini@gmail.com>
2024-03-06 18:19:20 +1000

    Add background image to page and body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,9 @@ export default async function Home() {
   return (
     <div className="relative flex items-center h-screen ">
       <div className="bg-pattern absolute top-0 left-0 w-full h-screen -z-10" />
+      <div className="fixed top-0 left-0 w-screen h-screen">
+        <Image src="/bg.webp" alt="background" quality="100" layout="fill" />
+      </div>
       <div className="relative max-w-md py-10 px-6 mx-4 sm:mx-auto backdrop-blur-[1px] rounded-3xl">
         <div className="absolute top-0 left-0 w-full h-full -z-10 rounded-7xl opacity-50 " />
         <h1 className="text-center text-4xl font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1] ">
