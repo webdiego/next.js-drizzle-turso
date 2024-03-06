cfeee259f313ac56e1571f251d25c0ed91d122e6
Diego Massarni <webdiegomassarini@gmail.com>
2024-03-06 17:53:09 +1000

    Add background pattern

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={"bg-gray-50"}>{children}</body>
+      <body className={""}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,17 @@ export default async function Home() {
   const allNotes = await db.select().from(notes).all();
 
   return (
-    <div className="max-w-md py-10 mx-4 sm:mx-auto ">
-      <h1 className="text-center text-4xl font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1] mt-24">
-        Template Next.js + Drizzle + Turso + Shadcn
-      </h1>
-      <p className="max-w-[750px] text-center text-lg text-muted-foreground ">
-        It includes a simple CRUD app for notes using server actions.
-      </p>
-      {/* <div className="flex  items-center justify-center space-x-2 py-2">
+    <div className="relative flex items-center h-screen ">
+      <div className="bg-pattern absolute top-0 left-0 w-full h-screen -z-10" />
+      <div className="relative max-w-md py-10 px-6 mx-4 sm:mx-auto backdrop-blur-[1px] rounded-3xl">
+        <div className="absolute top-0 left-0 w-full h-full -z-10 rounded-7xl opacity-50 " />
+        <h1 className="text-center text-4xl font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1] ">
+          Template Next.js + Drizzle + Turso + Shadcn
+        </h1>
+        <p className="max-w-[750px] text-center text-lg text-muted-foreground ">
+          It includes a simple CRUD app for notes using server actions.
+        </p>
+        {/* <div className="flex  items-center justify-center space-x-2 py-2">
         <Image
           src="/drizzle.png"
           alt="logo"
@@ -39,8 +42,9 @@ export default async function Home() {
           className="rounded-lg"
         />
       </div> */}
-      <Form />
-      <Notes notes={allNotes} />
+        <Form />
+        <Notes notes={allNotes} />
+      </div>
     </div>
   );
 }
